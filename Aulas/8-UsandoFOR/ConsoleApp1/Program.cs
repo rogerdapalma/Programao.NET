27da94d51eb2992e52a55bac24dcf53b51880c3a
Roger <rogerdapalma@gmail.com>
2025-03-14 12:34:09 -0300

    add 8

---
+++ Aulas/8-UsandoFOR/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("i vale : " + i);
+                if (i == 7) break; 
+            }
+            Console.WriteLine("-----------------------------------------------");
+            for (i = 10; i > 0; i--)
+            
+                if (i == 5)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("i vale : " + i);
+                }
+            }
+
+
+        }
+    }
