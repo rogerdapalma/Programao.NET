a5cb5c4f115527b5e31cd465506aa5bd341c6704
roger <rogerdapalma@gmail.com>
2025-03-21 12:40:34 -0300

    add aula 11

--- Aulas/11-AtividadePOO/11-AtividadePOO/Program.cs
+++ Aulas/11-AtividadePOO/11-AtividadePOO/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Livro l = new Livro();
+            Console.WriteLine("====================================");
+            Console.WriteLine("digite o titulo");
+            l.titulo = Console.ReadLine();
+            Console.WriteLine("digite o autor");
+            l.autor = Console.ReadLine();
+            Console.WriteLine("ano de fabricação");
+            l.anoPuclibado = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("titulo:"+l.titulo);
+            Console.WriteLine("autor:" + l.autor);
+            Console.WriteLine("ano de fabricação:" + l.anoPuclibado);
+            Console.WriteLine("====================================");
+            Livro l2 = new Livro();
+
+            Console.WriteLine("digite o titulo");
+            l2.titulo = Console.ReadLine();
+            Console.WriteLine("digite o autor");
+            l2.autor = Console.ReadLine();
+            Console.WriteLine("ano de fabricação");
+            l2.anoPuclibado = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("titulo:" + l2.titulo);
+            Console.WriteLine("autor:" + l2.autor);
+            Console.WriteLine("ano de fabricação:" + l2.anoPuclibado);
+            Console.WriteLine("====================================");
+            Livro l3 = new Livro();
+
+            Console.WriteLine("digite o titulo");
+            l3.titulo = Console.ReadLine();
+            Console.WriteLine("digite o autor");
+            l3.autor = Console.ReadLine();
+            Console.WriteLine("ano de fabricação");
+            l3.anoPuclibado = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("titulo:" + l3.titulo);
+            Console.WriteLine("autor:" + l3.autor);
+            Console.WriteLine("ano de fabricação:" + l3.anoPuclibado);
+            Console.WriteLine("====================================");
+        }   
     }
 }
